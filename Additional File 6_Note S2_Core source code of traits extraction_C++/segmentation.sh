066e36c6198bdb82c43d78c30ba754669df19fc6
diwu861125 <55370401+diwu861125@users.noreply.github.com>
2021-01-25 18:33:45 +0800

    ok

--- Additional File 6_Note S2_Core source code of traits extraction_C++/segmentation.sh
+++
@@ -1,4 +0,0 @@
-cd #!/usr/bin/env sh
-set -e
-
-python /SegNet_RiceCulm/test_segmentation_RiceCulm.py --model /SegNet_RiceCulm/SegNet_RiceCulm_deploy.prototxt --weights /SegNet_RiceCulm/test_weights_RiceCulm.caffemodel --data="/SegNet_RiceCulm/Sample/test.txt"
